5b5ee4542f7babb1502a8f3741a36e56dbf9cf57
Mihail292 <58395790+Mihail292@users.noreply.github.com>
2019-12-05 18:50:28 +0600

    Update index.js

--- index.js
+++ index.js
@@ -15,6 +15,8 @@
 	</head>
 	<body>
 
+  jhjhjhjhj
+
   <div style="position:absolute;display:block;top:0%;width:0%;text-align:center;font-family:arial;font-size:25px;color:#ffff00;"><span id="info">0</span></div>
   <div style="position:absolute;display:block;top:10%;width:0%;text-align:center;font-family:arial;font-size:25px;color:#ffff00;"><span id="info2">0</span></div>
   <div style="position:absolute;display:block;top:20%;width:0%;text-align:center;font-family:arial;font-size:25px;color:#ffff00;"><span id="info3">0</span></div>
